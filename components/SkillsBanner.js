be828594421e7b53e6bd5d0c8f7e2cd0edb6e743
Damien Aries <damien.aries@gmail.com>
2021-12-31 13:30:44 -0800

    saving progress - skills banner

--- components/SkillsBanner.js
+++ components/SkillsBanner.js
@@ -1,18 +1,38 @@
-import parse from 'html-react-parser';
+import ReactHtmlParser from 'react-html-parser';
+import { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import { SiMaterialUi, SiAmazonaws, SiReact, SiHtml5, SiCss3, SiNodeDotJs, SiNextDotJs, SiJavascript, SiTypescript, SiMongodb, SiTailwindcss, SiFirebase, SiStyledComponents, SiFigma } from 'react-icons/si';
 
 export default function SkillsBanner({ skills }) {
     // TODO generate dynamic icon imports + make icon string into component
-    console.log(skills);
+    const [htmlIconString, setHtmlIconString] = useState([]);
+    // class HtmlComponent extends React.Component {
+    //     render() {
+    //       const html = '<div>Example HTML string</div>';
+    //       return <div>{ ReactHtmlParser(html) }</div>;
+    //     }
+    //   }
+
+      // parse icon string into html
+      useEffect(() => {
+        const iconString = ['<ul>']
+        skills.forEach(skill => iconString.push(`<li><${skill} className="skill" /></li>`));
+        iconString.push('</ul>');
+        
+        setHtmlIconString(htmlIconString.join(''));
+
+        return htmlIconString;
+      }, [skills])
+      
+      console.log(htmlIconString);
 
     return (
         <StyledSkills>
-            <span className="skill-scrolling">
+           {/* <span className="skill-scrolling">
                 {
                     skills && skills.map(s => parse(`<${s} className="skill />`) )
                 }
-            </span>
+            </span>*/}
             <span className="skill-scrolling">
                 <SiHtml5 className="skill" />
                 <SiCss3 className="skill" />
