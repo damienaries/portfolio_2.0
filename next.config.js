17ed1956993b7ddbac2ab77403a281448bbd2e27
damienaries <damien.aries@gmail.com>
2026-02-05 19:11:05 -0800

    build errors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	// Explicitly use webpack instead of Turbopack
+	webpack: (config, { isServer }) => {
+		return config;
+	},
+	// Add empty turbopack config to silence the warning
+	turbopack: {},
 	compiler: {
 		emotion: {
 			sourceMap: true,
